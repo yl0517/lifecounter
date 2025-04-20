1b5ab1301a9c7648e668d23200069bbb101ced57
yl0517 <yl0517@uw.edu>
2025-04-19 20:49:10 -0700

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Yoobin Lee on 4/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ lifecounterTests/lifecounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  lifecounterTests.swift
+//  lifecounterTests
+//
+//  Created by Yoobin Lee on 4/19/25.
+//
+
+import Testing
+@testable import lifecounter
+
+struct lifecounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
